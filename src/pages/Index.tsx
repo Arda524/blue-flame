f1f12ea1a7bf8b40b8426ddedb8f6afd8adfba42
Ardawan M.Amin <ardawan524@gmail.com>
2025-08-22 12:11:25 +0300

    Add files via upload

---
+++ src/pages/Index.tsx
@@ -0,0 +1,13 @@
+import FireballCanvas from "@/components/FireballCanvas";
+import UI from "@/components/UI";
+
+const Index = () => {
+  return (
+    <div className="relative min-h-screen bg-black">
+      <FireballCanvas />
+      <UI />
+    </div>
+  );
+};
+
+export default Index;
